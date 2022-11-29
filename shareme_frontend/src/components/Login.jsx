04bed3fb471bcb0e2775099a178e99a5e77d5d80
Jordan Epps <jordanepps@gmail.com>
2022-11-29 14:13:56 -0500

    Style login page and add google oauth

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
 
 const Login = () => {
-  return <div>Login</div>;
+  const createOrGetUser = async (response, addUser) => {
+    const decoded = jwt_decode(response.credential);
+    console.log(decoded);
+  };
+  return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <div className="flex justify-start items-center flex-col h-screen">
+        <div className="relative w-full h-full">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+            <div className="p-5">
+              <img src={logo} width="130px" alt="logo" />
+            </div>
+            <div className="shadow-2xl">
+              <GoogleLogin
+                onSuccess={(response) => createOrGetUser(response)}
+                onError={(error) => console.log(error)}
+              />
+              {/* <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
 };
 
 export default Login;
