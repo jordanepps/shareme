7394986a4b8896f444906b5467550d2d62442749
Jordan Epps <jordanepps@gmail.com>
2022-11-29 16:53:27 -0500

    added user profile link to bottom of sidebar

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -18,7 +18,8 @@ const categories = [
   { name: 'Coding' },
 ];
 
-const Sidebar = (user, closeToggle) => {
+const Sidebar = ({ user }, closeToggle) => {
+  console.log(user);
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
   };
@@ -60,6 +61,16 @@ const Sidebar = (user, closeToggle) => {
           ))}
         </div>
       </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
+        >
+          <img src={user.image} className="w-10 h-10 rounded-full" alt="user" />
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   );
 };
