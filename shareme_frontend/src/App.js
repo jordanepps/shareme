8573b85b3bd6050e424d5567f0048b79981115fd
Jordan Epps <jordanepps@gmail.com>
2022-11-29 11:42:59 -0500

    Added routes for home and login components

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Routes, Route, useNavigate} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './container/Home'
+
 
 const App = () => {
   return (
-    <div>Helloooo</div>
+    <Routes>
+        <Route path="login" element={<Login />}/>
+        <Route path="/*" element={<Home />}/>
+    </Routes>
   )
 }
 
