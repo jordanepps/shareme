b0d3bdf7322182d21779468d81a91293c38831b0
Jordan Epps <jordanepps@gmail.com>
2022-11-30 09:53:06 -0500

    Fixed user profile link on pin

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -121,7 +121,7 @@ const Pin = ({ pin: { image, postedBy, _id, destination, save } }) => {
         )}
       </div>
       <Link
-        to={`user-profile/${user?._id}`}
+        to={`user-profile/${postedBy?._id}`}
         className="flex gap-2 mt-2 items-center"
       >
         <img
